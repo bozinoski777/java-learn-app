4f46daae770ff3e3861cf50b85119e6f7d8e70f0
Hristian Bozinoski <konten.bozinoski@gmail.com>
2022-05-23 14:24:40 +0200

    sample config

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+    def about
+    end
+  
+    def contact
+    end
+  
+    def home
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
+# config/routes.rb
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
 end
