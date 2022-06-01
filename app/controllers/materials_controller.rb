17d79098e7f5aaddce6df726eba88db02a0200de
Hristian Bozinoski <konten.bozinoski@gmail.com>
2022-06-01 17:15:08 +0200

    next phase

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -2,7 +2,15 @@ class MaterialsController < ApplicationController
     def index
         @materials = Material.all
         @reportcard = Reportcard.new
+        @images = {"Java Building Blocks": "https://res.cloudinary.com/dj9iphc8u/image/upload/c_scale,w_1456/v1653941157/Sopra_Steria/fernando-hernandez-efzwcMRM6j4-unsplash_g51ogr.jpg",
+                    "Operators and Statements": "https://res.cloudinary.com/dj9iphc8u/image/upload/c_scale,w_1043/v1653941156/Sopra_Steria/danial-igdery-FCHlYvR5gJI-unsplash_hvi31q.jpg",
+                    "Core Java APIs": "https://res.cloudinary.com/dj9iphc8u/image/upload/c_scale,w_899/v1653941148/Sopra_Steria/simon-abrams-k_T9Zj3SE8k-unsplash_svsrma.jpg",
+                    "Methods and Encapsulation": "https://res.cloudinary.com/dj9iphc8u/image/upload/c_scale,w_1132/v1653412708/Sopra_Steria/glenn-carstens-peters-npxXWgQ33ZQ-unsplash_u6kfap.jpg",
+                    "Class Design": "https://res.cloudinary.com/dj9iphc8u/image/upload/c_scale,w_970/v1653941149/Sopra_Steria/arif-riyanto-vJP-wZ6hGBg-unsplash_imyzld.jpg",
+                    "Exceptions": "https://res.cloudinary.com/dj9iphc8u/image/upload/c_scale,w_1140/v1653941153/Sopra_Steria/sigmund-Im_cQ6hQo10-unsplash_s55hou.jpg"}
+        @reportcards= Reportcard.all
     end
+
 
     def new
         @reportcard = Reportcard.new
